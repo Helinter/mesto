81c8603f016dcfbceb59de091157a113c8afb2e5
Igor Daragan <Helinter@yandex.ru>
2023-04-24 18:19:59 +0400

    ошибки после 1 ревью

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //popup
-let popupInputFirst = document.querySelector('.popup__input-first');
-let popupInputSecond = document.querySelector('.popup__input-second');
+let popupInputFirst = document.querySelector('.popup__input_type_name');
+let popupInputSecond = document.querySelector('.popup__input_type_job');
 let profileTitle = document.querySelector('.profile__info-title');
 let profileSubtitle = document.querySelector('.profile__info-subtitle');
 
@@ -11,12 +11,12 @@ let openPopup = document.querySelector('.profile__info-edit-button');
 let popup = document.querySelector('.popup');
 
 openPopup.addEventListener('click', function () {
-  popup.className = 'popup_open';
+  popup.classList.add('popup_opened');
 });
 
 let closePopup = document.querySelector('.popup__container-close-button');
 closePopup.addEventListener('click', function () {
-  popup.className = 'popup';
+  popup.classList.remove('popup_opened');
   profileTitle.textContent = popupInputFirst.value = profileTitle.textContent;
   popupInputSecond.value = profileSubtitle.textContent;
 });
@@ -31,12 +31,12 @@ let formElement = document.querySelector('.popup__container-button');
 function formSubmitHandler(evt) {
   evt.preventDefault();
 
-  let nameInput = document.querySelector('.popup__input-first');
-  let jobInput = document.querySelector('.popup__input-second');
+  let nameInput = document.querySelector('.popup__input_type_name');
+  let jobInput = document.querySelector('.popup__input_type_job');
 
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
-  popup.className = 'popup';
+  popup.classList.remove('popup_opened');
 }
 
 formElement.addEventListener('click', formSubmitHandler);
