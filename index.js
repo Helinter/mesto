fd589bce6f8f3f6b147ac796bac88230a4fa61e7
Igor Daragan <Helinter@yandex.ru>
2023-05-03 19:27:08 +0400

    pr5 лайки и лайки новым карточкам

--- index.js
+++ index.js
@@ -115,19 +115,39 @@ function addCard(evt) {
   const cardTemplate = document.querySelector('.card').content;
   const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
 
-initialCards.splice(initialCards.length, 0, {name: '', link: ''});
-initialCards[initialCards.length - 1].name = placeInput.value;
-initialCards[initialCards.length - 1].link = linkInput.value;
+  initialCards.splice(initialCards.length, 0, { name: '', link: '' });
+  initialCards[initialCards.length - 1].name = placeInput.value;
+  initialCards[initialCards.length - 1].link = linkInput.value;
 
   cardElement.querySelector('.element__title').textContent = initialCards[initialCards.length - 1].name;
   const titleImage = cardElement.querySelector('.element__image');
   titleImage.setAttribute('src', initialCards[initialCards.length - 1].link);
   titleImage.setAttribute('alt', initialCards[initialCards.length - 1].name);
-  
   cardsContainer.prepend(cardElement);
   popupCard.classList.remove('popup_opened');
   placeInput.value = 'Название';
   linkInput.value = 'Ссылка на картинку';
+
 }
 
 formCardElement.addEventListener('click', addCard);
+
+function like() {
+  const likeButton = document.querySelector('.element__like-button');
+    likeButton.addEventListener("click", function () {
+      likeButton.classList.toggle("element__like-button_active");
+    });
+  }
+formCardElement.addEventListener('click', like);
+
+
+function likes() {
+  const likeButton = document.querySelectorAll('.element__like-button');
+  for (let i = 0; i < likeButton.length; i++) {
+    likeButton[i].addEventListener("click", function () {
+      likeButton[i].classList.toggle("element__like-button_active");
+    });
+  }
+}
+
+likes();
